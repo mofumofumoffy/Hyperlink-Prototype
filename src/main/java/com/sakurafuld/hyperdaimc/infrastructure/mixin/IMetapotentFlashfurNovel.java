a098bd162afa0303320a40ad45a920f98e943cee
Moffy5612 <moffy3318rami@gmail.com>
2026-05-18 16:09:49 +0900

    専用対策除去

--- src/main/java/com/sakurafuld/hyperdaimc/infrastructure/mixin/IMetapotentFlashfurNovel.java
+++
@@ -1,9 +0,0 @@
-package com.sakurafuld.hyperdaimc.infrastructure.mixin;
-
-import flashfur.omnimobs.entities.metapotent_flashfur.MetapotentFlashfurEntity;
-
-public interface IMetapotentFlashfurNovel {
-    MetapotentFlashfurEntity hyperdaimc$getOriginal();
-
-    int hyperdaimc$getTime();
-}
